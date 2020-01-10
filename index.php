0475e363f78a7346b0323ccd0496a0a56f768145
manuruiner <manuel.ruiner@gmail.com>
2020-01-10 10:15:44 +0100

    new UI

--- index.php
+++ index.php
@@ -1,24 +1,35 @@
 <head>
   <title>Bootstrap Example</title>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="./css/main.css"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="./css/main.css">
+ 
+  
 </head>
 
 <body>
+  <div class="maincol">
   <header>
-    <h1 class="display-1">Visual Symbol Evaluation</h1>
+    <h1 class="h1">Visual Symbol Evaluation</h1>
   </header>
   
-  <main class="container-fluid">
-    <div class="row">
-      <div class="col-md-5 bg-light">
+  <main class="container-fluid maincol">
+    <div class="row maincol">
+      <div class="col-md-5 leftcol">
         <br>
-        <button type="button" class="btn btn-dark btn-primary">Upload Picture</button>
+       
+  <div id="msg"></div>
+  <form method="post" id="image-form">
+    <input type="file" name="img[]" class="file" accept="image/*">
+    <div class="input-group my-3">
+      <input type="text" class="form-control" disabled placeholder="Upload" id="file">
+      <div class="input-group-append">
+        <button type="button" class="browse btn btn-primary">Browse...</button>
+    
+    </div>
+  </form>
+</div>
         <br>
         <br>
       <div class="btn-group-lg mx-auto">
@@ -34,14 +45,32 @@
       <br>
       <br>
       <p>Verify:</p>
+      
+      <form>
+   <div>
+    <input type="radio" id="choice1"
+     name="contact" value="True">
+    <label for="choice1">True</label>
 
+    <input type="radio" id="choice2"
+     name="contact" value="False">
+    <label for="choice2">False</label>
+    <div>
+    <button class="btn-dark" type="submit">Submit</button>
+    </div>
+</div>
+  </form>
       </div>
     <div class="col-md-7">
-      <img src="..." class="img-fluid" alt="Responsive image">
-      Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+      <img src="https://placehold.it/800x600" id="preview" class="img-fluid" alt="Responsive image">
+</div>
     </div>
     </div>
   </main>
-
+  </div>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <script type="text/javascript" src="./js/upload.js"></script>
 </body>
 </html>
