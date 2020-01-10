d18ad35ff96aa0091c6dc7638a671bd1c0f85b5d
Alexander Hofmüller <alexander.hofmueller@stud.fh-campuswien.ac.at>
2020-01-10 17:00:02 +0100

    nothing much

--- py/eel_test.py
+++ py/eel_test.py
@@ -18,6 +18,6 @@ def say_hello_py(x):
     print('Hello from %s' % x)
 
 say_hello_py('Python World!')
-eel.say_hello_js('Python World!')   # Call a Javascript function
+#eel.say_hello_js('Python World!')   # Call a Javascript function
 
 eel.start('index.php', size=(650, 612))
